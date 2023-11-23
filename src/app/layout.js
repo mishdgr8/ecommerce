fc6f46a4f8be9832bc2cad24040d6c6463e5c976
mishdgr8 <Mishaelmordi@gmail.com>
2023-11-23 17:44:29 +0100

    admin, user logic complete, buttons complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import GlobalState from "@/app/context";
-import Navbar from "@/app/Navbar";
+import Navbar from "@/app/component/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/app/utils/index.js
@@ -0,0 +1,45 @@
+export const navOptions = [
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "listing",
+    label: "All Products",
+    path: "/product/listing/all-products",
+  },
+  {
+    id: "listingMen",
+    label: "Men",
+    path: "/product/listing/men",
+  },
+  {
+    id: "listingWomen",
+    label: "Women",
+    path: "/product/listing/women",
+  },
+  {
+    id: "listingKids",
+    label: "kids",
+    path: "/product/listing/kids",
+  },
+];
+
+export const adminNavOptions = [
+  {
+    id: "adminListing",
+    label: "Manage All Products",
+    path: "/admin-view/all-products",
+  },
+  {
+    id: "adminNewProduct",
+    label: "Add New Product",
+    path: "/admin-view/add-product",
+  },
+];
+
+export const styles = {
+  button:
+    "inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white",
+};
