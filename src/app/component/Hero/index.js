d5508cef39c05ccdff606743f927d44e0587cde9
mishdgr8 <Mishaelmordi@gmail.com>
2023-12-21 21:06:14 +0100

    update

--- src/app/component/Hero/index.js
+++ src/app/component/Hero/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { useEffect } from "react";
 
 const index = () => {
-  return <div></div>;
+  return <div> me and yiu know say nothing dey here</div>;
 };
 
 export default index;
